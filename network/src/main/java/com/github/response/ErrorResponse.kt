624f7c0eb8cf6c5ddd1bb253eb654dcb77f106ef
Yusuf Maulana <yusufmaulana@engineer.com>
2021-02-15 12:40:37 +0700

    - Update ErrorResponse.kt

--- network/src/main/java/com/github/response/ErrorResponse.kt
+++ network/src/main/java/com/github/response/ErrorResponse.kt
@@ -1,9 +1,13 @@
 package com.github.response
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @JsonClass(generateAdapter = true)
-data class ErrorResponse(val message: String = "") : Parcelable
+data class ErrorResponse(
+    val message: String = "",
+    @Json(name = "documentation_url") val url: String = ""
+) : Parcelable
